e642b48f59948f9af4ed006fc4de629daaa6b6d4
Sanjampreet Singh <sanjam531@gmail.com>
2022-12-22 17:13:38 +0530

    initail setup

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@chakra-ui/react";
+
+import Header from "./header/Header";
+
+export default function App() {
+  return (
+    <Container maxW="5xl">
+      <Header />
+    </Container>
+  );
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,48 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box>Logo</Box>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar
+                    size={"sm"}
+                    src={"https://avatars.dicebear.com/api/male/username.svg"}
+                  />
+                </MenuButton>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
